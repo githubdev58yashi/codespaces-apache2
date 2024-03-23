2216d971afd5db472c487fea0aa2d224bf40cc74
githubdev58yashi <80401287+githubdev58yashi@users.noreply.github.com>
2024-03-23 21:20:19 +0900

    Update postCreateCommand.sh

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,5 +1,5 @@
 echo ======postCreateCommand=====
-sudo sh -c \"yes | apt install apache2\"
+sudo sh -c "yes | apt install apache2"
 sudo rsync -r -v --delete /workspaces/codespaces-apache2/src /var/www/html
 sudo sed -e 's/Listen 80/Listen 127.0.0.1:3000/' -i /etc/apache2/ports.conf
 sudo sed -e '$a ServerName localhost' -i /etc/apache2/apache2.conf
