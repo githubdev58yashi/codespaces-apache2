c589449b3087ae25b585c4d0c46db48aa3025e84
githubdev58yashi <80401287+githubdev58yashi@users.noreply.github.com>
2024-03-24 07:36:34 +0000

    change sh

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo sh -c "yes | apt install apache2"
+sudo sh -c "yes | apt install lsyncd"
 
 # SSIの設定追加
 sudo sed -i -z 's/<Directory \/var\/www\/>\n\tOptions Indexes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n<\/Directory>/<Directory \/var\/www\/>\n\tOptions Includes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n\tAddtype text\/html \.txt\n\tAddOutputFilter INCLUDES \.html\n<\/Directory>/' /etc/apache2/apache2.conf
@@ -12,4 +13,7 @@ sudo sh -c "echo 'ServerName localhost' >> /etc/apache2/apache2.conf"
 sudo service apache2 start
 
 # ディレクトリの同期（権限周りが解決できないため、対象ソースをコピーする。）
-sudo rsync -r -v --delete /workspaces/codespaces-apache2/src /var/www/html
+# sudo rsync -r -v --delete /workspaces/codespaces-apache2/src /var/www/html
+
+# リアルタイム同期（権限周りが解決できないため、対象ソースをコピーする。）
+sudo lsyncd -delay 1 -direct /workspaces/codespaces-apache2/src/ /var/www/html/src
