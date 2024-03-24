c359129e782833e426f99d9501fdf540942d19c1
githubdev58yashi <80401287+githubdev58yashi@users.noreply.github.com>
2024-03-24 13:02:12 +0900

    Update postCreateCommand.sh

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,12 +1,12 @@
 sudo apt update
 sudo sh -c "yes | apt install apache2"
 
+# SSIの設定追加
+sudo sed -i -z 's/<Directory \/var\/www\/>\n\tOptions Indexes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n<\/Directory>/<Directory \/var\/www\/>\n\tOptions Includes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n\tAddtype text\/html \.txt\n\tAddOutputFilter INCLUDES \.html\n<\/Directory>/' /etc/apache2/apache2.conf
+
 # localhostのみがアクセスできるようにする。
 sudo sed -i 's/Listen 80/Listen 127.0.0.1:3000/' /etc/apache2/ports.conf
 sudo sed -e '$a ServerName localhost' -i /etc/apache2/apache2.conf
-
-# SSIの設定追加
-sudo sed -i -z 's/<Directory \/var\/www\/>\n\tOptions Indexes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n<\/Directory>/<Directory \/var\/www\/>\n\tOptions Includes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n\tAddtype text\/html \.txt\n\tAddOutputFilter INCLUDES \.html\n<\/Directory>/' /etc/apache2/apache2.conf
 
 # serviceコマンドじゃないと起動・停止とか出来ない。
 sudo service apache2 start
