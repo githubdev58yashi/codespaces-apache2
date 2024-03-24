6e33cf275d5d3e5f987cef82cd258c4cde9dd3fd
githubdev58yashi <80401287+githubdev58yashi@users.noreply.github.com>
2024-03-24 12:53:19 +0900

    Update postCreateCommand.sh

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,15 +1,16 @@
 sudo apt update
 sudo sh -c "yes | apt install apache2"
 
-# ports.conf
+# localhostのみがアクセスできるようにする。
 sudo sed -i 's/Listen 80/Listen 127.0.0.1:3000/' /etc/apache2/ports.conf
-
-# apache2.conf
 sudo sed -i '$a ServerName localhost' /etc/apache2/apache2.conf
+sed -n '1p' /etc/apache2/apache2.conf
+
+# SSIの設定追加
 sudo sed -i -z 's/<Directory \/var\/www\/>\n\tOptions Indexes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n<\/Directory>/<Directory \/var\/www\/>\n\tOptions Includes FollowSymLinks\n\tAllowOverride None\n\tRequire all granted\n\tAddtype text\/html \.txt\n\tAddOutputFilter INCLUDES \.html\n<\/Directory>/' /etc/apache2/apache2.conf
 
-# apacheの起動（ポート: 3000）
+# serviceコマンドじゃないと起動・停止とか出来ない。
 sudo service apache2 start
 
-# ディレクトリの同期
+# ディレクトリの同期（権限周りが解決できないため、対象ソースをコピーする。）
 sudo rsync -r -v --delete /workspaces/codespaces-apache2/src /var/www/html
